454857975146a27bf940a212cdece30f75ffc210
898444038 <898444038@qq.com>
2020-04-07 01:30:06 +0800

    commit

--- bs/src/main/java/com/ming/bs/BsApplication.java
+++ bs/src/main/java/com/ming/bs/BsApplication.java
@@ -4,15 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(
-	basePackages = {
-		"com.ming.bs"
-	}
-)
 @MapperScan(basePackages = {
 	"com.ming.bs.mapper",
 })
